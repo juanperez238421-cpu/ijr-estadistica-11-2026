6f8a50532e4d1c67f83236cda77242e5efff6593
juanperez238421-cpu <juanperez238421@gmail.com>
2026-08-19 08:19:28 -0500

    Configure Colab Lab 01 backend

---
+++ actividad-colab-01/config.js
@@ -0,0 +1,11 @@
+window.IJR_COLAB_ACTIVITY_CONFIG = Object.freeze({
+  activitySlug: 'statistics11-colab-basics-01-2026',
+  supabaseUrl: 'https://rlfxnjbqxbozjdzkbwlz.supabase.co',
+  supabaseAnonKey: 'sb_publishable_rmVOQ3Orx49KpW_4uMqYew_c2HpcA87',
+  sessionStorageKey: 'ijr-stat11-colab-basics-01-active-v1',
+  rpc: {
+    start: 'student_learning_activity_start',
+    resume: 'student_learning_activity_resume',
+    submit: 'student_learning_activity_submit'
+  }
+});
