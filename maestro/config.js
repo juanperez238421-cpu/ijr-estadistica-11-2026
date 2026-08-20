3b953a3daa4ed72f81db9fb3f8286e699aea0a3f
juanperez238421-cpu <juanperez238421@gmail.com>
2026-08-20 10:21:51 -0500

    Enable V10 master detail edit and delete RPCs

--- maestro/config.js
+++ maestro/config.js
@@ -2,5 +2,12 @@ window.IJR_MASTER_CONFIG = Object.freeze({
   supabaseUrl: 'https://rlfxnjbqxbozjdzkbwlz.supabase.co',
   supabaseAnonKey: 'sb_publishable_rmVOQ3Orx49KpW_4uMqYew_c2HpcA87',
   teacherSessionKey: 'ijr-stat11-master-teacher-session-v1',
-  rpc: {login:'teacher_code_login', logout:'teacher_code_logout', dashboard:'teacher_learning_activity_dashboard_v9'}
+  rpc: {
+    login:'teacher_code_login',
+    logout:'teacher_code_logout',
+    dashboard:'teacher_learning_activity_dashboard_v10',
+    detail:'teacher_learning_activity_detail_v10',
+    updateRegistration:'teacher_learning_activity_update_registration_v10',
+    deleteRegistration:'teacher_learning_activity_delete_v10'
+  }
 });
