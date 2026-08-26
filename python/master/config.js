10bf8514255c7fe17c7ee5177b38b8e1137fb7a9
juanperez238421-cpu <juanperez238421@gmail.com>
2026-08-26 14:02:48 -0500

    Add secure Python Hub master configuration

---
+++ python/master/config.js
@@ -0,0 +1,12 @@
+(() => {
+  'use strict';
+  const supabaseUrl='https://rlfxnjbqxbozjdzkbwlz.supabase.co';
+  window.IJR_PYTHON_MASTER_CONFIG=Object.freeze({
+    supabaseUrl,
+    supabasePublishableKey:'sb_publishable_rmVOQ3Orx49KpW_4uMqYew_c2HpcA87',
+    gatewayUrl:`${supabaseUrl}/functions/v1/teacher-auth-gateway`,
+    hubUrl:'https://juanperez238421-cpu.github.io/ijr-estadistica-11-2026/python/',
+    authStorageKey:'ijr-stat11-python-master-auth-v20',
+    refreshMs:20000
+  });
+})();
