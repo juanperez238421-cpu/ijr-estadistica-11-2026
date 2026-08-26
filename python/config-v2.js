9e9781c8c7246650bbdc327079f1972334ed1429
juanperez238421-cpu <juanperez238421@gmail.com>
2026-08-26 08:36:18 -0500

    Add gated Python hub Supabase config

---
+++ python/config-v2.js
@@ -0,0 +1,11 @@
+window.IJR_PYTHON_HUB_CONFIG = Object.freeze({
+  supabaseUrl: 'https://rlfxnjbqxbozjdzkbwlz.supabase.co',
+  supabasePublishableKey: 'sb_publishable_rmVOQ3Orx49KpW_4uMqYew_c2HpcA87',
+  institutionalEmailDomain: 'ijr.edu.co',
+  sessionStorageKey: 'ijr-stat11-python-hub-session-v2',
+  rpc: Object.freeze({
+    register: 'python_hub_register_v1',
+    resume: 'python_hub_resume_v1',
+    submit: 'python_hub_submit_v1'
+  })
+});
